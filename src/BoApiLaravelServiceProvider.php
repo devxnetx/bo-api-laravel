d09c718ebfa079251223796f160a07a4c20939a0
Lord George <devxnetx@gmail.com>
2023-08-20 09:51:54 +0300

    setup project for dev

---
+++ src/BoApiLaravelServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @author devxnet
+ * @date   2023-08-20 09:10:01
+ * @file   BoApiLaravelServiceProvider.php
+ *
+ * Do not modify this code on your own, as it may lead to unexpected outcomes.
+ * We do not take responsibility for any issues that may arise from unauthorized changes.
+ * For inquiries about modifications, please contact devxnetx@gmail.com.
+ */
+
+namespace Devxnetx\BoApiLaravel;
+
+use Illuminate\Support\ServiceProvider;
+
+class BoApiLaravelServiceProvider extends ServiceProvider
+{
+
+    public function boot ()
+    {
+    }
+
+    public function register ()
+    {
+        
+    }
+}
